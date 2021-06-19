8008af251c5bed9e1dfd761b45fcd6acf1ef38a3
Daigo Shitara <stain.witness+github@gmail.com>
2021-06-19 11:38:19 +0900

    Initial Commit

---
+++ Sound/SoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundApp.swift
+//  Sound
+//
+//  Created by shitara on 2021/06/19.
+//
+
+import SwiftUI
+
+@main
+struct SoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
